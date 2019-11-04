53b7b41fb2b58463643c5646aa5ea47886387414
Nick Hillary <hillan02@gmail.com>
2019-11-04 12:04:00 -0600

    added tests for registration

--- test/users-endpoints.spec.js
+++ test/users-endpoints.spec.js
@@ -5,7 +5,8 @@ const helpers = require('./test-helpers');
 describe.only('Users Endpoints', function() {
   let db;
 
-  const { testUsers } = helpers.makeArticlesFixtures();
+  const { testUsers, testThings } = helpers.makeThingsFixtures();
+  const testUser = testUsers[0];
 
   before('make knex instance', () => {
     db = knex({
@@ -23,7 +24,9 @@ describe.only('Users Endpoints', function() {
 
   describe(`POST /api/users`, () => {
     context(`User Validation`, () => {
-      beforeEach('insert users', () => helpers.seedUsers(db, testUsers));
+      beforeEach('insert users', () =>
+        helpers.seedThingsTables(db, testUsers, testThings)
+      );
 
       const requiredFields = ['user_name', 'password', 'full_name'];
 
@@ -44,6 +47,81 @@ describe.only('Users Endpoints', function() {
             .expect(400, {
               error: `Missing '${field}' in request body`
             });
+        });
+        it(`responds 400 'Password must be longer than 8 characters' when empty password`, () => {
+          const userShortPassword = {
+            user_name: 'test user_name',
+            password: '123456',
+            full_name: 'test full_name'
+          };
+          return supertest(app)
+            .post('/api/users')
+            .send(userShortPassword)
+            .expect(400, {
+              error: `Password must be longer than 8 characters`
+            });
+        });
+        it(`responds 400 'Password must be less than 72 characters' when long password`, () => {
+          const userLongPassword = {
+            user_name: 'test user_name',
+            password: `*`.repeat(73),
+            full_name: 'test full_name'
+          };
+          return supertest(app)
+            .post('/api/users')
+            .send(userLongPassword)
+            .expect(400, { error: `Password must be less than 72 characters` });
+        });
+        it(`responds 400 error when password starts with spaces`, () => {
+          const userPasswordStartsSpaces = {
+            user_name: 'test user_name',
+            password: ` 1Aa!2Bb@`,
+            full_name: 'test full_name'
+          };
+          return supertest(app)
+            .post('/api/users')
+            .send(userPasswordStartsSpaces)
+            .expect(400, {
+              error: `Password must not start or end with empty spaces`
+            });
+        });
+        it(`responds 400 error when password ends with spaces`, () => {
+          const userPasswordEndsSpaces = {
+            user_name: 'test user_name',
+            password: `1Aa!2Bb@ `,
+            full_name: 'test full_name'
+          };
+          return supertest(app)
+            .post('/api/users')
+            .send(userPasswordEndsSpaces)
+            .expect(400, {
+              error: `Password must not start or end with empty spaces`
+            });
+        });
+        it(`responds 400 error when password isn't complex enough`, () => {
+          const userPasswordnotComplex = {
+            user_name: 'test user_name',
+            password: `11AAaabb`,
+            full_name: 'test full_name'
+          };
+          return supertest(app)
+            .post('api/users')
+            .send(userPasswordnotComplex)
+            .expect(400, {
+              error: `Password must contain 1 upper case, lower case, number and special character`
+            });
+        });
+        it.only(`responds 400 'User name already taken' when user_name isn't unique`, () => {
+          const duplicateUser = {
+            user_name: testUser.user_name,
+            password: `11AAaa!!`,
+            full_name: 'test full_name'
+          };
+
+          return supertest(app)
+            .post('./api/users')
+            .send(duplicateUser)
+            .expect(400, { error: `username already taken` });
         });
       });
     });
